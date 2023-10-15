ff36e3d640fdcd6d089acbcd4521ef6fcae41c3c
Tyuiu.KalimullinaAH@outlook.com <boulevarovaalina@icloud.com>
2023-10-15 12:15:49 +0500

    Task6 Fix

--- Tyuiu.KalimullinaAH.Sprint2.Task6.V12.Lib/DataService.cs
+++ Tyuiu.KalimullinaAH.Sprint2.Task6.V12.Lib/DataService.cs
@@ -10,76 +10,44 @@ namespace Tyuiu.KalimullinaAH.Sprint2.Task6.V12.Lib
     {
         public string FindDateOfPreviousDay(int g, int m, int n)
         {
+            switch (n)
             {
-
-                switch (n)
-                {
-                    case 1:
-                        switch (m)
-                        {
-                            case 1:
-                                n = 31;
-                                m = 12;
-                                g--;
-                                break;
-                            default:
-                                m--;
-                                switch (m)
-                                {
-                                    case 1:
-                                    case 3:
-                                    case 5:
-                                    case 7:
-                                    case 8:
-                                    case 10:
-                                        n = 31;
-                                        break;
-                                    case 2:
-                                        n = 29;
-                                        break;
-                                    default:
-                                        n = 30;
-                                        break;
-                                }
-                                break;
-                        }
-                        break;
-                    default:
-                        if (n < 1 || n > 31)
-                        {
-                            return $"Количество дней от 1 до 31. Значение {n}";
-                        }
-                        else
-                        {
-                            n--;
+                case 1 when m == 1:
+                    n = 31;
+                    m = 12;
+                    g--;
+                    break;
+                case 1:
+                    m--;
+                    switch (m)
+                    {
+                        case 1:
+                        case 3:
+                        case 5:
+                        case 7:
+                        case 8:
+                        case 10:
+                            n = 31;
                             break;
-                        }
-
-                }
-
-                if (m < 10)
-                {
-                    if (n < 10)
+                        case 2:
+                            n = 29;
+                            break;
+                        default:
+                            n = 30;
+                            break;
+                    }
+                    break;
+                default:
+                    if (n < 1 || n > 31)
                     {
-                        return $"0{n}.0{m}.{g}";
+                        return $"Количество дней должно быть от 1 до 31. Значение {n}";
                     }
                     else
-                    {
-                        return $"{n}.0{m}.{g}";
-                    }
-                }
-                else
-                {
-                    if (n < 10)
-                    {
-                        return $"0{n}.{m}.{g}";
-                    }
-                    else
-                    {
-                        return $"{n}.{m}.{g}";
-                    }
-                }
+                        n--;
+                    break;
             }
+
+            return $"{n:D2}.{m:D2}.{g}";
         }
     }
 }
