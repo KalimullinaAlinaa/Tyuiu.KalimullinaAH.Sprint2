80047119b190fee384a17db5a8eda2fbc76c1817
Tyuiu.KalimullinaAH@outlook.com <boulevarovaalina@icloud.com>
2023-10-15 12:06:51 +0500

    Task6 Fix

--- Tyuiu.KalimullinaAH.Sprint2.Task6.V12.Test/DataServiceTest.cs
+++ Tyuiu.KalimullinaAH.Sprint2.Task6.V12.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.KalimullinaAH.Sprint2.Task6.V12.Test
         {
             DataService ds = new DataService();
 
-            Assert.AreEqual("4.4.2023", ds.FindDateOfPreviousDay(2023,4,5));
+            Assert.AreEqual("04.04.2023", ds.FindDateOfPreviousDay(2023,4,5));
             Assert.AreEqual("31.12.2021", ds.FindDateOfPreviousDay(2022, 1, 1));
             Assert.AreEqual("10.10.2023", ds.FindDateOfPreviousDay(2023, 10, 11));
             Assert.AreEqual("30.11.1999", ds.FindDateOfPreviousDay(1999, 12, 1));
